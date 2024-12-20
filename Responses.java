7cc2410b10aec37eeb43cd346619226af17ac72f
10trips <ethan.freckleton@gmail.com>
2024-12-20 08:54:12 -0600

    Create Responses.java

---
+++ Responses.java
@@ -0,0 +1,29 @@
+/**
+* This file contains a 2-d array of all possible responseds based on two keywords given.
+*/
+
+public class Responses {
+	private String[][] absoluteResponses {
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	};
+	
+	private String[] rowKeyWords {
+		"How",
+		
+	};
+	
+	private String[] collumnKeyWords {
+		"Doing",
+		
+	}
+}
