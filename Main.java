1315b47c26a535c796dd033afffc0babea31be09
10trips <ethan.freckleton@gmail.com>
2025-01-06 08:54:39 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,17 @@
-import java.util.*;
 
 public class Main {
 	//the sentence from the user
-	private String userSentence;
+	private static String input = "what can you do?";
 	public static void main(String[] args) {
+		String lowerinput = input.toLowerCase();
+		if (lowerinput.contains("what") && lowerinput.contains("you") && lowerinput.contains("doing")) {
+			System.out.println("nothin much hbu");
+		} else if (lowerinput.contains("how") && lowerinput.contains("you") && lowerinput.contains("doing")) {
+			System.out.println("good hbu");
+		} else if (lowerinput.contains("what") && lowerinput.contains("can") && lowerinput.contains("do")) {
+			System.out.println("i can talk or chill or more ;)");
+		}
 	}
 	//Splits the sentence into each of its words
-	public ArrayList<String> getWordsFromSentence()
-	{
-		ArrayList<String> words = new ArrayList<String>();
-		
-		return words;
-	}
+	
 }
