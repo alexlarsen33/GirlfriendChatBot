8505db03a23130c06d6cea98d6c2da1c2a84f48b
Alexander-Page-dot <alexpage2951@gmail.com>
2025-01-06 08:52:54 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,13 +2,21 @@ import java.util.*;
 
 public class Main {
 	//the sentence from the user
-	private String userSentence;
+	private static String userSentence;
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); //creates a scanner for user input
+		System.out.print(""); //opening statement goes here
+		
+		userSentence = input.nextLine();
+		
+		System.out.println("user input: " + userSentence);
+		
 	}
 	//Splits the sentence into each of its words
 	public ArrayList<String> getWordsFromSentence()
 	{
 		ArrayList<String> words = new ArrayList<String>();
+		
 		
 		return words;
 	}
