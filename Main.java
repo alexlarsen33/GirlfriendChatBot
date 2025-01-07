1ec84baefae935d1cfad2482dc6f7c19f4a309f4
Alexander-Page-dot <alexpage2951@gmail.com>
2025-01-07 08:24:18 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,25 +1,28 @@
-
+import java.util.*;
 public class Main {
 	//the sentence from the user
-	private static String input = "what can you do?";
+	private static String userInput = "what can you do?";
 	public static void main(String[] args) {
 
 		Scanner input = new Scanner(System.in); //creates a scanner for user input
 		System.out.print(""); //opening statement goes here
 		
-		userSentence = input.nextLine();
+		userInput = input.nextLine();
 		
 		
 
-		String lowerinput = input.toLowerCase();
+		String lowerinput = userInput.toLowerCase();
 		if (lowerinput.contains("what") && lowerinput.contains("you") && lowerinput.contains("doing")) {
 			System.out.println("nothin much hbu");
 		} else if (lowerinput.contains("how") && lowerinput.contains("you") && lowerinput.contains("doing")) {
 			System.out.println("good hbu");
 		} else if (lowerinput.contains("what") && lowerinput.contains("can") && lowerinput.contains("do")) {
 			System.out.println("i can talk or chill or more ;)");
+		} else if(lowerinput.contains("what") && lowerinput.contains("is") && lowerinput.contains("your") && lowerinput.contains("favorite") && lowerinput.contains("food")) {
+			System.out.println("Grapes");
 		}
+			
 	}
-	//Splits the sentence into each of its words
+
 	
 }
